9154d4d76b50dca0cd9982709b580f902f3c5c10
Sebastian <eljhoan1411@gmail.com>
2021-09-25 12:42:03 -0500

    correcion chicuadrado

--- Pruebas_uniformidad.py
+++ Pruebas_uniformidad.py
@@ -3,31 +3,36 @@ import math
 ChiCalculadoTabla = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 KolmogorovCalculado =  [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 FOA = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-def PruebaChiCuadrado(TablaFrecuencia):
+
+def PruebaChiCuadrado(TablaFrecuencia,datosGenerados):
     Xcritico = 16.92#float(input("Ingrese el chi critico: "))
+    ChiCalculado(TablaFrecuencia,datosGenerados)
+    DibujarTabla(TablaFrecuencia,Xcritico,datosGenerados)
     if sum(ChiCalculadoTabla) <= Xcritico:
-        DibujarTabla(TablaFrecuencia,Xcritico)
-        print("Segun la prueba de Chi cuadrado el generador es bueno en cuanto a uniformidad")
+        print("Segun la prueba de Chi cuadrado el generador es BUENO!!! en cuanto a uniformidad")
+        print("ChiCalculado es: ",sum(ChiCalculadoTabla))
+        print("Segun la prueba chiCuadrado: ChiCalculado <= chicritico")
+        print(sum(ChiCalculadoTabla),"<=",xc)
     else:
         print("Segun la prueba de Chi cuadrado el generador NO!!!!! es bueno en cuanto a uniformidad")
-
-def ChiCalculado(TablaFrecuencia):
+        print("ChiCalculado es: ",sum(ChiCalculadoTabla))
+        print("Segun la prueba chiCuadrado: ChiCalculado <= chicritico")
+        print(sum(ChiCalculadoTabla),">",xc)
+        
+def ChiCalculado(TablaFrecuencia,datosGenerados):
     for i in range(10):
-        ChiCalculadoTabla[i] = (100 - TablaFrecuencia[i])* (100 - TablaFrecuencia[i])/ 100
+        ChiCalculadoTabla[i] = (datosGenerados - TablaFrecuencia[i])* (datosGenerados - TablaFrecuencia[i])/ datosGenerados
 
 
-
-def DibujarTabla(TablaFrecuencia,xc):
+def DibujarTabla(TablaFrecuencia,xc,datosGenerados):
     getcontext()
     getcontext().prec = 1
     rango = 0
     print("  Rango  |   FE   |   FO     |  (FE - FO)^2 / FE | ")
     for i in range(10):
-        print(rango,"-" ,rango +Decimal(0.1)," | ",TablaFrecuencia[i]," | ","  100    |",ChiCalculadoTabla[i])
+        print(rango,"-" ,rango +Decimal(0.1)," | ",TablaFrecuencia[i]," | ",datosGenerados,"      |",ChiCalculadoTabla[i])
         rango =Decimal( rango + Decimal(0.1))
-    print("ChiCalculado es: ",sum(ChiCalculadoTabla))
-    print("Segun la prueba chiCuadrado: ChiCalculado <= chicritico")
-    print(sum(ChiCalculadoTabla),"<=",xc)
+
 
 def CalcKolmogorov(TablaFrecuencia,numerodatos):
     getcontext()
@@ -59,10 +64,6 @@ def CalcularFOA(TablaFrecuencia):
             FOA[i] = TablaFrecuencia[i]
         else:
             FOA [i] = TablaFrecuencia[i]+FOA[i-1]
-def ObtMenor():
-    for i in range(10):
-        if KolmogorovCalculado[i] > 0:
-            return i
 def DibujarKolmogorov(TablaFrecuencia,datosGenerados):
     getcontext()
     getcontext().prec = 1
