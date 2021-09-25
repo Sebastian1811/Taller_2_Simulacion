72f995f99c2ef3fc45b1905b78a32494f2fd0498
Sebastian <eljhoan1411@gmail.com>
2021-09-24 22:15:11 -0500

    cree funcion de dibujar tabla

--- Pruebas_uniformidad.py
+++ Pruebas_uniformidad.py
@@ -2,8 +2,9 @@ ChiCalculadoTabla = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 
 
 def PruebaChiCuadrado():
-    Xcritico = float(input("Ingrese el chi critico: "))
+    Xcritico = 16.92#float(input("Ingrese el chi critico: "))
     if sum(ChiCalculadoTabla) <= Xcritico:
+        DibujarTabla()
         print("Segun la prueba de Chi cuadrado el generador es bueno en cuanto a uniformidad")
     else:
         print("Segun la prueba de Chi cuadrado el generador NO!!!!! es bueno en cuanto a uniformidad")
@@ -11,3 +12,12 @@ def PruebaChiCuadrado():
 def ChiCalculado(TablaFrecuencia):
     for i in range(10):
         ChiCalculadoTabla[i] = (100 - TablaFrecuencia[i])* (100 - TablaFrecuencia[i])/ 100
+
+def DibujarTabla():
+    rango = 0
+    print("Rango | FE | FO | (FE - FO)^2 / FE | ")
+    for i in range(10):
+        print(rango )
+        print(rango +0.1)
+
+        rango = rango + 0.1
