7c35a00ceb9044140a4b00507dca04e6d6ab431c
Brayan Cortes <cortes.brayan@correounivalle.edu.co>
2021-09-25 11:09:13 -0500

    Menu con continuar

--- menu.py
+++ menu.py
@@ -1,17 +1,35 @@
-from generadores import *  
+from generadores import *
+contador = True
+while contador == True:
 
-print("############################ MENU ###################################")
-print("Se implementaron los generadores lineal y congruente con 9 grados de libertad, se generaron 1000 numeros pseudoaleatorios por cada generador")
+    print("############################ ANOTACIONES ###################################")
+    print("Se implementaron los generadores lineal y congruente con 9 grados de libertad, se generaron 1000 numeros pseudoaleatorios por cada generador")
 
-Puntero_Menu = int(input("Eliga una opcion:"))
+    print("############################ MENU ###################################")
+    print("Bienvenido al menu de las pruebas de independencia y uniformidad")
+    print("\n")
+    print("1.Generador lineal congruente con pruebas de uniformidad")
+    print("2.Generador de estandar minimo con pruebas de uniformidad")
+    print("3.Generador lineal congruente con pruebas de independencia")
+    print("4.Generador de estandar minimo con pruebas de independencia")
+    print("\n")
 
-if Puntero_Menu == 1:
-    print("Usted selecciono el generador lineal congruente con las pruebas de uniformidad")
-    generadorlincong()
-elif Puntero_Menu == 2:
-    print("Usted selecciono el generador de estandar minimo con las pruebas de uniformidad")
-    generadorestmin()
-elif Puntero_Menu == 3:
-    print("")
-else:
-    print("")
+    Puntero_Menu = int(input("Eliga una opcion:"))
+
+    if Puntero_Menu == 1:
+        print("Usted selecciono el generador lineal congruente con las pruebas de uniformidad")
+        generadorlincong()
+        Continuar = int(input("Desea continuar?:"))
+        print("Si?: presione 1")
+        print("No?: presione 2")
+        if Continuar == 1:
+            Puntero_Menu = int(input("Eliga una opcion:"))
+        else:
+            contador = False
+    elif Puntero_Menu == 2:
+        print("Usted selecciono el generador de estandar minimo con las pruebas de uniformidad")
+        generadorestmin()
+    elif Puntero_Menu == 3:
+        print("")
+    else:
+        print("")
