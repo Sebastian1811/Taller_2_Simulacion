088713868b19f667c879bc13424776f8cad31485
Brayan Cortes <cortes.brayan@correounivalle.edu.co>
2021-09-24 21:49:45 -0500

    Se coloco el generadorlincong_Faltaprueba

--- menu.py
+++ menu.py
@@ -6,7 +6,8 @@ print("Se implementaron los generadores lineal y congruente con 9 grados de libe
 Puntero_Menu = int(input("Eliga una opcion:"))
 
 if Puntero_Menu == 1:
-    print("Usted selecciono el generador")
+    print("Usted selecciono el generador lineal congruente con las pruebas de uniformidad")
+    generadorlincong()
 elif Puntero_Menu == 2:
     print("")
 elif Puntero_Menu == 3:
