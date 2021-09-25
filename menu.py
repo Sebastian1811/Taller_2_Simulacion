3c080f40e8d7afbac199be7f03d711b640f37c1a
Brayan Cortes <cortes.brayan@correounivalle.edu.co>
2021-09-24 21:31:41 -0500

    Creacion del menu e importe de generadores

---
+++ menu.py
@@ -0,0 +1,15 @@
+from generadores import *
+
+print("############################ MENU ###################################")
+print("Se implementaron los generadores lineal y congruente con 9 grados de libertad, se generaron 1000 numeros pseudoaleatorios por cada generador")
+
+Puntero_Menu = int(input("Eliga una opcion:"))
+
+if Puntero_Menu == 1:
+    print("Usted selecciono el generador")
+elif Puntero_Menu == 2:
+    print("")
+elif Puntero_Menu == 3:
+    print("")
+else:
+    print("")
