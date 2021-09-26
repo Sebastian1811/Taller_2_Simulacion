900725faf863faefe12407d4488ac2b07c3564fb
Sebastian <eljhoan1411@gmail.com>
2021-09-25 19:03:09 -0500

    Agregue funcion de periodo y visualizar recurrencias y datos

--- generadores.py
+++ generadores.py
@@ -1,5 +1,8 @@
 import Pruebas_uniformidad as Prueba
+
 Tabla = [0,0,0,0,0,0,0,0,0,0]
+datosGenerados = list()
+recurrencias = list()
 n = 0
 def EvaluarRecurrencia(Xn,m):
     Rn = Xn/m
@@ -27,45 +30,56 @@ def returnTabla():
     return Tabla
 def returnN():
     return n
-def ReiniciarTabla():
-    for i in range(10):
-        Tabla[i] = 0
+def ReiniciarArreglos(Arreglo):
+    for i in range(len(Arreglo)):
+        Arreglo[i] = 0
+def ReiniciarListas(lista):
+    while lista != []:
+        lista.pop()
 
-def generadorlincong():
+def generadorlincong(Datosrequeridos):
     a = int(input("a es: "))
     c = int(input("c es: "))
     m = int(input("m es: "))
     Xo = int(input("Xo es: "))
     global n
-    n = 1000#int(input(" cuantos datos desea generar: "))
+    n = Datosrequeridos #int(input(" cuantos datos desea generar: "))
     print()
     #print(Xo)
+    datosGenerados.append(Xo)
+    recurrencias.append(Xo/m)
     res = (a * Xo + c) % m
     EvaluarRecurrencia(res,m)
     #print(res)
     for i in range(n-1):
         res = (a * res + c) % m
+        datosGenerados.append(res)
+        recurrencias.append(res/m)
         EvaluarRecurrencia(res,m)
         #print(res)
 
-    #Prueba.PruebaChiCuadrado(Tabla,n)
-
-    #Prueba.PruebaKolmogorov(Tabla,n)
-
-def generadorestmin():
+def generadorestmin(Datosrequeridos):
     a = int(input("a es: "))
     m = int(input("m es: "))
     Xo = int(input("Xo es: "))
     global n
-    n = 1000#int(input(" cuantos datos desea generar: "))
+    n = Datosrequeridos#int(input(" cuantos datos desea generar: "))
     print()
-    print(Xo)
+    #print(Xo)
     res = (a * Xo) % m
+    datosGenerados.append(Xo)
+    recurrencias.append(Xo/m)
     EvaluarRecurrencia(res,m)
     print(res)
     for i in range(n-1):
         res = (a * res) % m
+        datosGenerados.append(res)
+        recurrencias.append(res/m)
         EvaluarRecurrencia(res,m)
-    #Prueba.ChiCalculado(Tabla)
-    #Prueba.PruebaChiCuadrado(Tabla)
-        #print(res)
+
+def HallarPeriodo():
+    Periodo = datosGenerados.index(datosGenerados[0],1) + 1
+    return Periodo
+def VerDatosGenerados(datos):
+    for i in range(len(datos)):
+        print(datos[i])
