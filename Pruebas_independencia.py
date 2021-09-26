1e933af80bae70daa49d0a82a4c4775c027ee3fc
Sebastian <eljhoan1411@gmail.com>
2021-09-26 16:34:54 -0500

    funcion para mapear recurrencias en pares

--- Pruebas_independencia.py
+++ Pruebas_independencia.py
@@ -1,5 +1,12 @@
 import math
 corridas = list()
+TablaSeries = [
+[0,0,0,0,0]
+[0,0,0,0,0]
+[0,0,0,0,0]
+[0,0,0,0,0]
+[0,0,0,0,0]
+]
 def ContarCorridas(recurrencias ):
     Corridas = 0
     ValCorrida = 0
@@ -73,8 +80,20 @@ def mostrarCorridas(corridas,titulo):
     print("////////////////////////////////////////////////////")
 
 
+def MapearPares(recurrencias):
+    listaPares = list()
+    count = 0
+    for i in range(len(recurrencias)-1):
+        if count < (len(recurrencias)-1):
+            listaPares.append([recurrencias[count],recurrencias[count+1]])
+            count+=2
+            #print(count)
+    return listaPares
+#def pruebaSeries(recurrencias):
+
+
 # /////////// PRUEBAS ////////////
-"""array1 = [0.41, 0.68, 0.89, 0.94, 0.74, 0.91, 0.55, 0.62, 0.36, 0.27,
+array1 = [0.41, 0.68, 0.89, 0.94, 0.74, 0.91, 0.55, 0.62, 0.36, 0.27,
 0.19, 0.72, 0.75 ,0.08, 0.54, 0.02, 0.01, 0.36, 0.16, 0.28,
 0.18, 0.01, 0.95, 0.69, 0.18, 0.47, 0.23, 0.32, 0.82, 0.53,
 0.31, 0.42, 0.73, 0.04, 0.83, 0.45, 0.13, 0.57, 0.63, 0.29
@@ -83,5 +102,9 @@ array2 = [0.08, 0.09, 0.23, 0.29, 0.42, 0.55, 0.58, 0.72, 0.89, 0.91,
 0.11, 0.16, 0.18, 0.31, 0.41, 0.53, 0.71, 0.73, 0.74, 0.84,
 0.01, 0.09, 0.30, 0.32, 0.45, 0.47, 0.69, 0.74, 0.91, 0.95,
 0.12, 0.13, 0.29, 0.36, 0.38, 0.54, 0.68, 0.86, 0.88, 0.91]
-PruebaCorridas(array1,40)
+#PruebaCorridas(array1,40)
 #PruebaCorridas(array1,40)"""
+
+"""print(MapearPares(array1))
+print(len(MapearPares(array1)))
+"""
