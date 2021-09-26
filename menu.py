07714ce61290c10b54da38327ffea84dfbf021c4
Brayan Cortes <cortes.brayan@correounivalle.edu.co>
2021-09-26 14:59:52 -0500

    Menu "terminado" 3.0

--- menu.py
+++ menu.py
@@ -96,15 +96,15 @@ def Prubas_Uniformidad_independencia():
                             Puntero_Interno4 = int(input("Escoga una opcion:"))
                         while 1:
                             if Puntero_Interno4 == 1:
-                                #Mostrar Prueba de corrida
+                                #Mostrar Prueba de corrida aqui
                                 print("Mostrar prueba de corrida")
                                 break
                             elif Puntero_Interno4 == 2:
-                                #Mostrar prueba de serie
+                                #Mostrar prueba de serie aqui
                                 print("Prueba de serie")
                                 break
                             elif  Puntero_Interno4 == 3:
-                                #Mostrar prueba de poker
+                                #Mostrar prueba de poker aqui
                                 print("Prueba de poker")
                                 break
                             elif Puntero_Interno4 == 4:
@@ -112,7 +112,7 @@ def Prubas_Uniformidad_independencia():
                     elif Puntero_Interno3 == 2:
                         break
                 elif Puntero_Interno2 == 2:
-                    #Prueba de kolmogorov
+                    #Prueba de kolmogorov aqui
                     print("\n")
                     print("/////////PRUEBA DE KOLMOGOROV////////////////")
                     print("\n")
@@ -136,15 +136,15 @@ def Prubas_Uniformidad_independencia():
                             Puntero_Interno4 = int(input("Escoga una opcion:"))
                         while 1:
                             if Puntero_Interno4 == 1:
-                                #Mostrar Prueba de corrida
+                                #Mostrar Prueba de corrida aqui
                                 print("Mostrar prueba de corrida")
                                 break
                             elif Puntero_Interno4 == 2:
-                                #Mostrar prueba de serie
+                                #Mostrar prueba de serie aqui
                                 print("Prueba de serie")
                                 break
                             elif  Puntero_Interno4 == 3:
-                                #Mostrar prueba de poker
+                                #Mostrar prueba de poker aqui
                                 print("Prueba de poker")
                                 break
                             elif Puntero_Interno4 == 4:
@@ -154,7 +154,97 @@ def Prubas_Uniformidad_independencia():
                 elif Puntero_Interno2 == 3: 
                     break
         elif Puntero_Interno == 2:
-            print("Estandar minimo")
+            print("######################## Bienvenido al menu del generador Estandar minimo ####################################")
+            print("1.Prueba de x2")
+            print("2.Prueba kolmogorov")
+            print("3.Volver")
+            print("############################################################")
+            Puntero_Interno2 = int(input("Eliga una opcion:"))
+            while (Puntero_Interno2 > 3 or Puntero_Interno2 < 1):
+                Puntero_Interno2 = int(input("Eliga una opcion:"))
+            while 1:
+                if Puntero_Interno2 == 1:
+                    #Prueba del chi cuadrado aqui
+                    print("\n")
+                    print("/////////PRUEBA DE chi cuadrado////////////////")
+                    print("\n")
+                    print("######################## Bienvenido al menu de las pruebas de independencia ####################################")
+                    print("Que desea hacer ahora?")
+                    print("1.Pruebas de Independencia")
+                    print("2.volver")
+                    print("######################## ############################################## ####################################")
+                    Puntero_Interno3 = int(input("Escoga una opcion:"))
+                    while (Puntero_Interno3 > 2 or Puntero_Interno3 < 1 ):
+                        Puntero_Interno3 = int(input("Escoga una opcion:"))
+                    if Puntero_Interno3 == 1:
+                        print("######################## Bienvenido al menu de las pruebas de independencia ####################################")
+                        print("1.Prueba de corrida")
+                        print("2.Prueba de serie")
+                        print("3.Prueba de poker")
+                        print("4.Volver")
+                        print("##############################################################################################################")
+                        Puntero_Interno4 = int(input("eliga una opcion:"))
+                        while (Puntero_Interno4 > 4 or Puntero_Interno4 < 1 ):
+                            Puntero_Interno4 = int(input("Escoga una opcion:"))
+                        while 1:
+                            if Puntero_Interno4 == 1:
+                                #Mostrar Prueba de corrida aqui
+                                print("Mostrar prueba de corrida")
+                                break
+                            elif Puntero_Interno4 == 2:
+                                #Mostrar prueba de serie aqui
+                                print("Prueba de serie")
+                                break
+                            elif  Puntero_Interno4 == 3:
+                                #Mostrar prueba de poker aqui
+                                print("Prueba de poker")
+                                break
+                            elif Puntero_Interno4 == 4:
+                                break
+                    elif Puntero_Interno3 == 2:
+                        break
+                elif Puntero_Interno2 == 2:
+                    #Prueba de kolmogorov aqui
+                    print("\n")
+                    print("/////////PRUEBA DE KOLMOGOROV////////////////")
+                    print("\n")
+                    print("######################## Bienvenido al menu de las pruebas de independencia ####################################")
+                    print("Que desea hacer ahora?")
+                    print("1.Pruebas de Independencia")
+                    print("2.volver")
+                    print("######################## ############################################## ####################################")
+                    Puntero_Interno3 = int(input("Escoga una opcion:"))
+                    while (Puntero_Interno3 > 2 or Puntero_Interno3 < 1 ):
+                        Puntero_Interno3 = int(input("Escoga una opcion:"))
+                    if Puntero_Interno3 == 1:
+                        print("######################## Bienvenido al menu de las pruebas de independencia ####################################")
+                        print("1.Prueba de corrida")
+                        print("2.Prueba de serie")
+                        print("3.Prueba de poker")
+                        print("4.Volver")
+                        print("##############################################################################################################")
+                        Puntero_Interno4 = int(input("eliga una opcion:"))
+                        while (Puntero_Interno4 > 4 or Puntero_Interno4 < 1 ):
+                            Puntero_Interno4 = int(input("Escoga una opcion:"))
+                        while 1:
+                            if Puntero_Interno4 == 1:
+                                #Mostrar Prueba de corrida aqui
+                                print("Mostrar prueba de corrida")
+                                break
+                            elif Puntero_Interno4 == 2:
+                                #Mostrar prueba de serie aui
+                                print("Prueba de serie")
+                                break
+                            elif  Puntero_Interno4 == 3:
+                                #Mostrar prueba de poker aui
+                                print("Prueba de poker")
+                                break
+                            elif Puntero_Interno4 == 4:
+                                break
+                    elif Puntero_Interno3 == 2:
+                        break     
+                elif Puntero_Interno2 == 3: 
+                    break
         elif  Puntero_Interno == 3:
             break        
             
