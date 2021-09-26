1dbca890f66da41fb0284942ba4a97b35d190951
Brayan Cortes <cortes.brayan@correounivalle.edu.co>
2021-09-25 22:53:24 -0500

    Menu Pruebas_Uniformidad e independencia

--- menu.py
+++ menu.py
@@ -16,7 +16,6 @@ def probar_generadores():
         while (Puntero_Interno > 3 or Puntero_Interno < 1):
             Puntero_Interno = int(input("Eliga una opcion:"))
 
-
         if Puntero_Interno == 1:
             print("######################## Bienvenido al menu del generador lineal congruente ####################################")
             print("1.Mostrar recurrencia")
@@ -51,6 +50,72 @@ def probar_generadores():
             break
 
 
+def Prubas_Uniformidad_independencia():
+    while True:
+        print("Se corre con 1000 datos generados y con 9 grados de libertad")
+        print("############################# Bienvenido al menu de pruebas de uniformidad ###############################")
+        print("\n")
+        print("1.Generador lineal congruente")
+        print("2.Generador estandar minimo")
+        print("3.Volver")
+        print("############################################################")
+        Puntero_Interno = int(input("Elija una opcion"))
+        while (Puntero_Interno > 3 or Puntero_Interno < 1):
+            Puntero_Interno = int(input("Eliga una opcion:"))
+        if Puntero_Interno == 1:
+            print("######################## Bienvenido al menu del generador lineal congruente ####################################")
+            print("1.Prueba de x2")
+            print("2.Prueba kolmogorov")
+            print("3.Volver")
+            print("############################################################")
+            Puntero_Interno2 = int(input("Eliga una opcion:"))
+            while (Puntero_Interno2 > 3 or Puntero_Interno2 < 1):
+                Puntero_Interno2 = int(input("Eliga una opcion:"))
+            if Puntero_Interno2 == 1:
+                #Prueba del chi cuadrado aqui
+                print("Que desea hacer ahora?")
+                print("1.Pruebas de Independencia")
+                print("2.volver")
+                Puntero_Interno3 = int(input("Escoga una opcion"))
+                while (Puntero_Interno3 > 2 or Puntero_Interno3 < 1 ):
+                    Puntero_Interno3 = int(input("Escoga una opcion"))
+                if Puntero_Interno3 == 1:
+                    print("######################## Bienvenido al menu de las pruebas de independencia ####################################")
+                    print("1.Prueba de corrida")
+                    print("2.Prueba de serie")
+                    print("3.Prueba de poker")
+                    print("4.Volver")
+                    Puntero_Interno4 = int(input("eliga una opcion:"))
+                    while (Puntero_Interno4 > 4 or Puntero_Interno4 < 1 ):
+                        Puntero_Interno4 = int(input("Escoga una opcion"))
+                    while 1:
+                        if Puntero_Interno4 == 1:
+                            #Mostrar Prueba de corrida
+                            print("Mostrar prueba de corrida")
+                            break
+                        elif Puntero_Interno4 == 2:
+                            #Mostrar prueba de serie
+                            print("Prueba de serie")
+                            break
+                        elif  Puntero_Interno4 == 3:
+                            #Mostrar prueba de poker
+                            print("Prueba de poker")
+                            break
+                        elif Puntero_Interno4 == 4:
+                            break
+                elif Puntero_Interno3 == 2:
+                    break
+            elif Puntero_Interno2 == 2:
+                print("Kolmogorov")  
+            elif Puntero_Interno2 == 3: 
+                break
+        elif Puntero_Interno == 2:
+            print("Estandar minimo")
+        elif  Puntero_Interno == 3:
+            break        
+            
+        
+
 
 
 while contador == True:
@@ -58,7 +123,7 @@ while contador == True:
     print("Bienvenido al menu de las pruebas de independencia y uniformidad")
     print("\n")
     print("1.Probar generadores")
-    print("2.Generador de estandar minimo con pruebas de uniformidad")
+    print("2.Pruebas de uniformidad e independencia")
     print("3.Generador lineal congruente con pruebas de independencia")
     print("4.Generador de estandar minimo con pruebas de independencia")
     print("\n")
@@ -68,6 +133,8 @@ while contador == True:
         Puntero_Menu = int(input("Eliga una numero entero entre 1 y 4 no es tan dificil:"))
     if Puntero_Menu == 1:
         probar_generadores()
+    elif Puntero_Menu == 2:
+        Prubas_Uniformidad_independencia()
 """
     elif Puntero_Menu == 2:
         print("Usted selecciono el generador de estandar minimo con las pruebas de uniformidad")
