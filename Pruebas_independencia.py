0a01e77de22d8ae437a90df7ca298493a770007b
Sebastian <eljhoan1411@gmail.com>
2021-09-26 14:32:58 -0500

    prueba de idependencia implementada

--- Pruebas_independencia.py
+++ Pruebas_independencia.py
@@ -1,9 +1,5 @@
-
-def PruebaCorridas(recurrencias ):
-    Varianza = (2000-1)/3
-    Media = ((16*1000)-29)/90
-    Z = 1.96
-    Zo = -1.96
+import math
+def ContarCorridas(recurrencias ):
     Corridas = 0
     ValCorrida = 0
     pos = 0
@@ -31,7 +27,23 @@ def PruebaCorridas(recurrencias ):
             Corridas+=1
     return Corridas
 
-"""print(PruebaCorridas([0.08, 0.09, 0.23, 0.29, 0.42, 0.55, 0.58, 0.72, 0.89, 0.91,
+def PruebaCorridas(recurrencias):
+    Varianza = (2000-1)/3
+    Media = ((16*1000)-29)/90
+    Z = 1.96
+    Zo = -1.96
+    hipotesis = 0
+    Corridas = ContarCorridas(recurrencias)
+    numerador = Corridas - Varianza
+    denominador = math.sqrt(Media)
+    Zobs =  numerador / denominador
+    if Zobs < Z and Zobs > Zo:
+        hipotesis = 1
+    if hipotesis:
+        print("aprobado master")
+    else:
+        print("se rechaza")
+"""PruebaCorridas([0.08, 0.09, 0.23, 0.29, 0.42, 0.55, 0.58, 0.72, 0.89, 0.91,
 0.11, 0.16, 0.18, 0.31, 0.41, 0.53, 0.71, 0.73, 0.74, 0.84,
 0.01, 0.09, 0.30, 0.32, 0.45, 0.47, 0.69, 0.74, 0.91, 0.95,
-0.12, 0.13, 0.29, 0.36, 0.38, 0.54, 0.68, 0.86, 0.88, 0.91]))"""
+0.12, 0.13, 0.29, 0.36, 0.38, 0.54, 0.68, 0.86, 0.88, 0.91])"""
